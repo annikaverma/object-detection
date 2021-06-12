0d65acf129fb33dc3613729012f5563c2549fe9d
annikaverma <76493450+annikaverma@users.noreply.github.com>
2021-06-12 18:17:12 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
 
 function preload(){
-    img = loadImage('dog_cat.jpg');
+  img = loadImage('dog_cat.jpg');
 }
 
 
 function setup() {
-    canvas = createCanvas(640, 420);
-    canvas.center();
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
 }
 
 function draw() {
@@ -16,4 +36,10 @@ function draw() {
   noFill();
   stroke(255, 0, 0);
   rect(30, 60, 450, 350 );
+
+  fill("#FF0000");
+  text("cat", 320, 120);
+  noFill();
+  stroke("#FF0000");
+  rect(300, 90, 270, 320 );
 }
